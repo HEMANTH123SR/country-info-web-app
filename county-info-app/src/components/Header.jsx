b90a383179331d13ba42600d838583e65a0e0824
HEMANTH123SR <applicationcomputer257@gmail.com>
2023-11-01 19:36:14 +0530

    responsive header content

--- county-info-app/src/components/Header.jsx
+++ county-info-app/src/components/Header.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import { BsMoonFill } from "react-icons/bs";
 
 function Header() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
   return (
-    <div>Header</div>
-  )
+    <div
+      className={`${
+        isDarkMode ? "bg-gray-800" : "bg-slate-300"
+      } h-16 flex flex-row items-center justify-between px-20`}
+    >
+      <h1
+        className={`${isDarkMode ? "text-slate-300" : "text-gray-700"} 
+        text-2xl`}
+      >
+        Where in the world?
+      </h1>
+
+      <button
+        onClick={() => {
+          setIsDarkMode((prev) => !prev);
+        }}
+        className={`${isDarkMode ? "text-slate-300" : "text-gray-700"} flex items-center`}
+      >
+        <BsMoonFill className="mr-2" /> {`${isDarkMode ? "Light Mode" : "Dark Mode"}`}
+      </button>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
